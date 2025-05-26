38ff6c29e9867dd897e51424e89d83db848e819a
ryan <ryan.kimani0@gmail.com>
2025-05-26 22:26:16 +0300

    Search box, search bar, background image Done Tentatively and started off on location box

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,9 +5,29 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/"
 }
 function App() {
+  
+  const datebuilder =  (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay]
+  }
+
   return (
     <div className="app">
       <main>
+        <div className="search-box">
+          <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          />
+        </div>
+        <div className='location-box'>
+          <div className='location'>Nairobi, Kenya</div>
+          <div className='date'>(dateBuilder(new Date()))</div>
+
+        </div>
       </main>
     </div>
   );
