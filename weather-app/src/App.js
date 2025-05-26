16a8870578481414dda4481dfbf0c47a04936760
ryan <ryan.kimani0@gmail.com>
2025-05-26 21:54:34 +0300

    Created React app and added resources

---
+++ weather-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const api = {
+  key: "8e47847af179c12b7af114ca39d77102",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+function App() {
+  return (
+    <div className="app">
+      <main>
+      </main>
+    </div>
+  );
+}
+
+export default App;
